669cd2cd88d93ae520c2ba974eca83c25fc8f822
Timm Knape <timm@knp.de>
2020-06-01 12:25:32 +0200

    more generic io

--- index.md
+++ index.md
@@ -18,6 +18,31 @@
 
 ```
 @def(globals)
+	void put(char ch) {
+		std::cout.put(ch);
+	}
+	void put(const char *begin, const char *end) {
+		if (begin && begin < end) {
+			while (begin != end) {
+				put(*begin++);
+			}
+		}
+	}
+	void put(const char *begin) {
+		if (begin) {
+			while (*begin) {
+				put(*begin++);
+			}
+		}
+	}
+	int get() {
+		return std::cin.get();
+	}
+@end(globals)
+```
+
+```
+@add(globals)
 	@put(needed by write addr)
 	static void write_addr(
 		const char *addr
@@ -29,14 +54,19 @@
 
 ```
 @def(main)
-	char *addr {
-		reinterpret_cast<char *>(write_addr)
-	};
+	char *addr { reinterpret_cast<char *>(
+		write_addr
+	) };
 	for (;;) {
 		write_addr(addr);
-		std::cout << "> ";
-		std::cout << "unknown command\n";
-		break;
+		put("> ");
+		int cmd { get() };
+		if (cmd == EOF) { 
+			put("quit\n");
+			break;
+		}
+		@put(command switch)
+		put("unknown command\n");
 	}
 @end(main)
 ```
@@ -52,13 +82,19 @@
 
 ```
 @def(needed by write hex byte)
-	static void write_hex_nibble(int nibble) {
-		static const char digits[] = "0123456789abcdef";
-		static_assert(sizeof(digits) == 16 + 1);
+	static void write_hex_nibble(
+		int nibble
+	) {
+		static const char digits[] {
+			"0123456789abcdef"
+		};
+		static_assert(
+			sizeof(digits) == 16 + 1
+		);
 		if (nibble >= 0 && nibble <= 15) {
-			std::cout << digits[nibble];
+			put(digits[nibble]);
 		} else {
-			std::cout << '?';
+			put('?');
 		}
 	}
 @end(needed by write hex byte)
@@ -70,18 +106,85 @@
 		write_hex_nibble(byte >> 4);
 		write_hex_nibble(byte & 0xf);
 	} else {
-		std::cout << "??";
+		put("??");
 	}
 @end(write hex byte)
 ```
 
 ```
 @def(write addr)
-	unsigned long value { reinterpret_cast<unsigned long>(addr) };
-	std::cout << '$';
+	char buffer[8 * 1024];
+	unsigned long value {
+		reinterpret_cast<unsigned long>(
+			buffer
+		)
+	};
 	int shift { (sizeof(long) - 1) * 8 };
 	for (; shift >= 0; shift -= 8) {
-		write_hex_byte((value >> shift) & 0xff);
+		write_hex_byte(
+			(value >> shift) & 0xff
+		);
 	}
 @end(write addr)
+```
+
+```
+@def(command switch)
+	if (cmd == '\n' || cmd == '\r') {
+		@put(dump hex)
+		continue;
+	}
+@end(command switch)
+```
+
+```
+@def(dump hex)
+	for (
+		int lines { 8 }; lines; --lines
+	) {
+		constexpr int bytes_per_row {
+			16
+		};
+		write_addr(addr);
+		put(": ");
+		@put(dump hex part)
+		put("| ");
+		@put(dump text part)
+		put('\n');
+		addr += bytes_per_row;
+	}
+@end(dump hex)
+```
+
+```
+@def(dump hex part) {
+	int row { 0 };
+	for (; row < bytes_per_row; ++row) {
+		write_hex_byte(addr[row] & 0xff);
+		put(' ');
+		@mul(pad dump)
+	}
+} @end(dump hex part)
+```
+
+```
+@def(dump text part) {
+	int row { 0 };
+	for (; row < bytes_per_row; ++row) {
+		if (isprint(addr[row])) {
+			put(addr[row]);
+		} else {
+			put('.');
+		}
+		@mul(pad dump)
+	}
+} @end(dump text part)
+```
+
+```
+@def(pad dump)
+	if (row + 1 != bytes_per_row &&
+		row % 8 == 7
+	) { put(' '); }
+@end(pad dump)
 ```
--- mon.cpp
+++ mon.cpp
@@ -11,55 +11,90 @@
 	
 #line 20 "index.md"
 
-	
+	void put(char ch) {
+		std::cout.put(ch);
+	}
+	void put(const char *begin, const char *end) {
+		if (begin && begin < end) {
+			while (begin != end) {
+				put(*begin++);
+			}
+		}
+	}
+	void put(const char *begin) {
+		if (begin) {
+			while (*begin) {
+				put(*begin++);
+			}
+		}
+	}
+	int get() {
+		return std::cin.get();
+	}
+
 #line 45 "index.md"
 
 	
-#line 54 "index.md"
+#line 75 "index.md"
 
-	static void write_hex_nibble(int nibble) {
-		static const char digits[] = "0123456789abcdef";
-		static_assert(sizeof(digits) == 16 + 1);
+	
+#line 84 "index.md"
+
+	static void write_hex_nibble(
+		int nibble
+	) {
+		static const char digits[] {
+			"0123456789abcdef"
+		};
+		static_assert(
+			sizeof(digits) == 16 + 1
+		);
 		if (nibble >= 0 && nibble <= 15) {
-			std::cout << digits[nibble];
+			put(digits[nibble]);
 		} else {
-			std::cout << '?';
+			put('?');
 		}
 	}
 
-#line 46 "index.md"
+#line 76 "index.md"
 
 	static void write_hex_byte(int byte) {
 		
-#line 68 "index.md"
+#line 104 "index.md"
 
 	if (byte >= 0 && byte <= 255) {
 		write_hex_nibble(byte >> 4);
 		write_hex_nibble(byte & 0xf);
 	} else {
-		std::cout << "??";
+		put("??");
 	}
 
-#line 48 "index.md"
+#line 78 "index.md"
 
 	}
 
-#line 21 "index.md"
+#line 46 "index.md"
 
 	static void write_addr(
 		const char *addr
 	) {
 		
-#line 79 "index.md"
+#line 115 "index.md"
 
-	unsigned long value { reinterpret_cast<unsigned long>(addr) };
-	std::cout << '$';
+	char buffer[8 * 1024];
+	unsigned long value {
+		reinterpret_cast<unsigned long>(
+			buffer
+		)
+	};
 	int shift { (sizeof(long) - 1) * 8 };
 	for (; shift >= 0; shift -= 8) {
-		write_hex_byte((value >> shift) & 0xff);
+		write_hex_byte(
+			(value >> shift) & 0xff
+		);
 	}
 
-#line 25 "index.md"
+#line 50 "index.md"
 
 	}
 
@@ -67,16 +102,90 @@
 
 	int main() {
 		
-#line 31 "index.md"
+#line 56 "index.md"
 
-	char *addr {
-		reinterpret_cast<char *>(write_addr)
-	};
+	char *addr { reinterpret_cast<char *>(
+		write_addr
+	) };
 	for (;;) {
 		write_addr(addr);
-		std::cout << "> ";
-		std::cout << "unknown command\n";
-		break;
+		put("> ");
+		int cmd { get() };
+		if (cmd == EOF) { 
+			put("quit\n");
+			break;
+		}
+		
+#line 132 "index.md"
+
+	if (cmd == '\n' || cmd == '\r') {
+		
+#line 141 "index.md"
+
+	for (
+		int lines { 8 }; lines; --lines
+	) {
+		constexpr int bytes_per_row {
+			16
+		};
+		write_addr(addr);
+		put(": ");
+		
+#line 160 "index.md"
+ {
+	int row { 0 };
+	for (; row < bytes_per_row; ++row) {
+		write_hex_byte(addr[row] & 0xff);
+		put(' ');
+		
+#line 185 "index.md"
+
+	if (row + 1 != bytes_per_row &&
+		row % 8 == 7
+	) { put(' '); }
+
+#line 165 "index.md"
+
+	}
+} 
+#line 150 "index.md"
+
+		put("| ");
+		
+#line 171 "index.md"
+ {
+	int row { 0 };
+	for (; row < bytes_per_row; ++row) {
+		if (isprint(addr[row])) {
+			put(addr[row]);
+		} else {
+			put('.');
+		}
+		
+#line 185 "index.md"
+
+	if (row + 1 != bytes_per_row &&
+		row % 8 == 7
+	) { put(' '); }
+
+#line 179 "index.md"
+
+	}
+} 
+#line 152 "index.md"
+
+		put('\n');
+		addr += bytes_per_row;
+	}
+
+#line 134 "index.md"
+
+		continue;
+	}
+
+#line 68 "index.md"
+
+		put("unknown command\n");
 	}
 
 #line 8 "index.md"
